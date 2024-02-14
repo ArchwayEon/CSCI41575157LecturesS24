fad565982625d8226e5c82b9059c035bb938e04a
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2024-02-14 06:10:09 -0500

    3d cube

--- CSCI41575157LecturesS24/Main.cpp
+++ CSCI41575157LecturesS24/Main.cpp
@@ -34,6 +34,7 @@ struct Result {
 
 struct VertexData {
     glm::vec3 position, color;
+    glm::vec2 tex;
 };
 
 static void Log(std::stringstream& log, const std::vector<char>& message)
@@ -188,11 +189,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     glfwSetFramebufferSizeCallback(window, OnWindowSizeChanged);
     //glfwMaximizeWindow(window);
 
+    // Cull back faces and use counter-clockwise winding of front faces
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LEQUAL);
+    glDepthRange(0.0f, 1.0f);
+
     std::string vertexSource =
         "#version 430\n"
         "layout(location = 0) in vec3 position;\n"
         "layout(location = 1) in vec3 color;\n"
+        "layout(location = 2) in vec2 texCoord;\n"
         "out vec4 fragColor;\n"
+        "out vec2 fragTexCoord;\n"
         "uniform mat4 world;\n"
         "uniform mat4 view;\n"
         "uniform mat4 projection;\n"
@@ -200,15 +214,19 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         "{\n"
         "   gl_Position = projection * view * world * vec4(position, 1.0);\n"
         "   fragColor = vec4(color, 1.0);\n"
+        "   fragTexCoord = texCoord;\n"
         "}\n";
 
     std::string fragmentSource =
         "#version 430\n"
         "in vec4 fragColor;\n"
+        "in vec2 fragTexCoord;\n"
         "out vec4 color;\n"
+        "uniform sampler2D tex;\n"
         "void main()\n"
         "{\n"
-        "   color = fragColor;\n"
+        "   vec4 texFragColor = texture(tex, fragTexCoord) * fragColor;\n"
+        "   color = texFragColor;\n"
         "}\n";
 
     unsigned int shaderProgram;
@@ -226,13 +244,77 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     unsigned int worldLoc = glGetUniformLocation(shaderProgram, "world");
 
-    VertexData vertexData[6]{};
-    vertexData[0] = { {-5.0f, 5.0f, 0.0f}, {1.0f, 0.0f, 0.0f} };
-    vertexData[1] = { {-5.0f,-5.0f, 0.0f}, {1.0f, 0.0f, 0.0f} };
-    vertexData[2] = { { 5.0f,-5.0f, 0.0f}, {1.0f, 0.0f, 0.0f} };
-    vertexData[3] = { {-5.0f, 5.0f, 0.0f}, {0.0f, 0.0f, 1.0f} };
-    vertexData[4] = { { 5.0f,-5.0f, 0.0f}, {0.0f, 0.0f, 1.0f} };
-    vertexData[5] = { { 5.0f, 5.0f, 0.0f}, {0.0f, 0.0f, 1.0f} };
+    // Create the texture data
+    unsigned char* textureData = new unsigned char[] {
+          0, 0, 0, 255, 0, 0, 255, 255, 0, 0, 255, 255, 0, 0, 0, 255,
+          0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 255, 0, 255,
+          0, 255, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 255, 0, 255,
+          0, 0, 0, 255, 255, 0, 0, 255, 255, 0, 0, 255, 0, 0, 0, 255
+    };
+
+    // Generate the texture id
+    GLuint textureId;
+    glGenTextures(1, &textureId);
+    // Select the texture 
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    // Apply texture parameters 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    // Send the texture to the GPU 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 4, 4, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+    // Generate mipmaps
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    // Front face
+    VertexData A = { {-5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}};
+    VertexData B = { {-5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}};
+    VertexData C = { { 5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}};
+    VertexData D = { { 5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}};
+    // Right face
+    VertexData E = { { 5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}};
+    VertexData F = { { 5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}};
+    VertexData G = { { 5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}};
+    VertexData H = { { 5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f}};
+    // Back face
+    VertexData I = { { 5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f} };
+    VertexData J = { { 5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f} };
+    VertexData K = { {-5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f} };
+    VertexData L = { {-5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f} };
+    // Left face
+    VertexData M = { {-5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f} };
+    VertexData N = { {-5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f} };
+    VertexData O = { {-5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f} };
+    VertexData P = { {-5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f} };
+    // Top face
+    VertexData Q = { {-5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f} };
+    VertexData R = { {-5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f} };
+    VertexData S = { { 5.0f, 5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f} };
+    VertexData T = { { 5.0f, 5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f} };
+    // Bottom face
+    VertexData U = { { 5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f} };
+    VertexData V = { { 5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f} };
+    VertexData W = { {-5.0f,-5.0f, 5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f} };
+    VertexData X = { {-5.0f,-5.0f,-5.0f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f} };
+
+    // 3 vertex per triangle, 2 triangles per face, 6 faces
+    // 3 * 2 * 6 = 36 vertices
+    VertexData vertexData[36]{
+        // Front face
+        A, B, C, A, C, D,
+        // Right face
+        E, F, G, E, G, H,
+        // Back face
+        I, J, K, I, K, L,
+        // Left face
+        M, N, O, M, O, P,
+        // Top face
+        Q, R, S, Q, S, T,
+        // Bottom face
+        U, V, W, U, W, X
+    };
 
     unsigned int vaoId, vboId;
     glGenVertexArrays(1, &vaoId);
@@ -243,16 +325,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    float angle = 45;
-    float xPos = -10, yPos = 0;
-    float cameraX = -10, cameraY = 0;
-    float left = -10.0f;
-    float right = 10.0f;
-    float bottom = -10.0f;
-    float top = 10.0f;
+    float cubeYAngle = 0;
+    float cubeXAngle = 0;
+    float cubeZAngle = 0;
+    float left = -20.0f;
+    float right = 20.0f;
+    float bottom = -20.0f;
+    float top = 20.0f;
     int width, height;
-    float aspectRatio;
+    
     std::string message = result.message;
+
+    float aspectRatio;
+    float nearPlane = 1.0f;
+    float farPlane = 50.0f;
+    float fieldOfView = 60;
+
+    glm::vec3 cameraPosition(15.0f, 15.0f, 20.0f);
+    glm::vec3 cameraTarget(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
 
     glm::mat4 view;
     glm::mat4 projection;
@@ -264,24 +355,22 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         glfwGetWindowSize(window, &width, &height);
 
         glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-        referenceFrame = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
-        view = CreateViewMatrix(
-            glm::vec3(cameraX, cameraY, 1.0f),
-            glm::vec3(0.0f, 0.0f, -1.0f),
-            glm::vec3(0.0f, 1.0f, 0.0f)
-        );
+        referenceFrame = glm::rotate(glm::mat4(1.0f), glm::radians(cubeYAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+        referenceFrame = glm::rotate(referenceFrame, glm::radians(cubeXAngle), glm::vec3(1.0f, 0.0f, 0.0f));
+        referenceFrame = glm::rotate(referenceFrame, glm::radians(cubeZAngle), glm::vec3(0.0f, 0.0f, 1.0f));
+       
+        view = glm::lookAt(cameraPosition, cameraTarget, cameraUp);
+        
         if (width >= height) {
             aspectRatio = width / (height * 1.0f);
-            projection = glm::ortho(
-                left * aspectRatio, right * aspectRatio, bottom, top, -1.0f, 1.0f);
         }
         else {
             aspectRatio = height / (width * 1.0f);
-            projection = glm::ortho(
-                left, right, bottom * aspectRatio, top * aspectRatio, -1.0f, 1.0f);
         }
+        projection = glm::perspective(
+            glm::radians(fieldOfView), aspectRatio, nearPlane, farPlane);
 
         // Render the object
         if (result.isSuccess)
@@ -296,7 +385,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             EnableAttribute(0, 3, sizeof(VertexData), (void*)0);
             // Colors
             EnableAttribute(1, 3, sizeof(VertexData), (void*)sizeof(glm::vec3));
-            glDrawArrays(GL_TRIANGLES, 0, 6);
+            // Texture Coords
+            EnableAttribute(2, 2, sizeof(VertexData), (void*)(sizeof(glm::vec3)*2));
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, textureId);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
             glDisableVertexAttribArray(0);
             glDisableVertexAttribArray(1);
             glUseProgram(0);
@@ -311,9 +404,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
             1000.0f / io.Framerate, io.Framerate);
         ImGui::ColorEdit3("Background color", (float*)&clearColor.r);
-        ImGui::SliderFloat("Angle", &angle, 0, 360);
-        ImGui::SliderFloat("Camera X", &cameraX, left, right);
-        ImGui::SliderFloat("Camera Y", &cameraY, bottom, top);
+        ImGui::SliderFloat("X Angle", &cubeXAngle, 0, 360);
+        ImGui::SliderFloat("Y Angle", &cubeYAngle, 0, 360);
+        ImGui::SliderFloat("Z Angle", &cubeZAngle, 0, 360);
+        ImGui::SliderFloat("Camera X", &cameraPosition.x, left, right);
+        ImGui::SliderFloat("Camera Y", &cameraPosition.y, bottom, top);
         ImGui::End();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
