0172d5d2bab4e71881852758d46be1ce7c7e64b7
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2024-02-25 13:48:38 -0500

    Updated fragment shader

--- CSCI41575157LecturesS24/lighting.frag.glsl
+++ CSCI41575157LecturesS24/lighting.frag.glsl
@@ -40,18 +40,25 @@ void main()
    float distanceToLight = length(localLightPosition - fragPosition);
    float attenuation = 1.0 / 
       (1.0 + localLightAttenuationCoef * pow(distanceToLight, 2));
+   localDiffuse *= attenuation;
    if(materialShininess > 0.0){
       vec3 reflectDir = reflect(-toLocalLightDir, fragNormal);
       float angIncidence = max(dot(viewDir, reflectDir), 0.0f);
       float spec = pow(angIncidence, materialShininess);
       specular = materialSpecularIntensity * spec * 
         localLightIntensity * vec4(localLightColor, 1.0f); 
+      
+      vec3 globalReflectDir = reflect(-toGlobalLightDir, fragNormal);
+      angIncidence = max(dot(viewDir, globalReflectDir), 0.0f);
+      spec = pow(angIncidence, materialShininess);
+      specular += materialSpecularIntensity * spec * 
+        globalLightIntensity * vec4(globalLightColor, 1.0f);
    }
 
    vec4 texFragColor = texture(tex, fragTexCoord) * fragColor;
    vec4 ambientColor = materialAmbientIntensity * vec4(1.0f, 1.0f, 1.0f, 1.0f);
-   color = (ambientColor + globalDiffuse + 
-            (localDiffuse * attenuation) + specular) * texFragColor;
+   color = (ambientColor + globalDiffuse + localDiffuse + specular) *   
+            texFragColor;
 }
 
 vec4 calculateDiffuse(vec3 lightDir, vec3 unitNormal, float lightIntensity, vec3 lightColor)
